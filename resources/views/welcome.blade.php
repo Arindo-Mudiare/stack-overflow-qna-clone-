9314f964e9f4868723b93e9dd6447a19fe009ec9
Arindo <ArindoTmudiare@gmail.com>
2018-12-31 18:25:44 +0100

    created basic authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Stack Overflow Klone!!
                 </div>
 
                 <div class="links">
