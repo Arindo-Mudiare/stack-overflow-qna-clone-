0141453221d6d69553fba4793b49a145cdcc340b
Arindo <ArindoTmudiare@gmail.com>
2018-12-31 23:26:09 +0100

    Prepopulated users and questions tables with model factories

--- app/User.php
+++ app/User.php
@@ -31,9 +31,5 @@ class User extends Authenticatable
     public function questions() {
         return $this->hasMany(Question::class);
     }
-    // mutator function
-    public function setTitleAttribute($value) {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
+    
 }
